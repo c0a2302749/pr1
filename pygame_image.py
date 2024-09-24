36adec711c839290dd94c13f1c0d234667ad01a4
C0A23027 <c0a2302749@edu.teu.ac.jp>
2024-09-24 15:46:08 +0900

    [v0.1.6] edit pygame_image.py > change bg

--- pygame_image.py
+++ pygame_image.py
@@ -9,18 +9,24 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
+
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    tutb_img = pg.image.load("fig/3.png")
-    tutb_img = pg.transform.flip(tutb_img, True, False)
+
+    bg_img2 = pg.transform.flip(bg_img, True, False)
+
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        bg_x=-(tmr%800)
+        screen.blit(bg_img, [bg_x, 0])
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(tutb_img, [300, 200])
+        screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
+        
         clock.tick(200)
 
 
