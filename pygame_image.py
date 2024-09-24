a9e2dbdd4ce1d424304568d10af984dcf2856d7b
C0A23027 <c0a2302749@edu.teu.ac.jp>
2024-09-24 16:59:04 +0900

    [v0.2.2] edit pygame_image.py > refactor: kk_rct.move_ip

--- pygame_image.py
+++ pygame_image.py
@@ -25,16 +25,18 @@ def main():
         bg_x=-(tmr%3200)
         #こうかとんの移動
         key_lst=pg.key.get_pressed()
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
+        kk_x,kk_y=0,0
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0, 1)
+            kk_y=1
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip(0, -1)
+            kk_y=-1
+        if key_lst[pg.K_LEFT]:
+            kk_x=-1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(2, 0)
+            kk_x=2
         else:
-            kk_rct.move_ip(-1, 0)
+            kk_x=-1
+        kk_rct.move_ip(kk_x, kk_y)
 
 
 
